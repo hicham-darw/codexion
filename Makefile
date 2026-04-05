04685265e0c9dfd187c96397dbabe86a97eab79e
hicham-darw <185743161+hicham-darw@users.noreply.github.com>
2026-04-05 02:03:33 +0100

    renamed helper function fix error destroy

--- Makefile
+++ Makefile
@@ -2,7 +2,11 @@ CC = cc
 CFLAGS = -Wall -Wextra -Werror
 LDFLAGS = -pthread
 NAME = codexion
-SRC = $(wildcard *.c)
+SRC = coder.c codexion.c compile.c create_threads.c destroy.c dongle.c \
+		free.c heap.c heapify.c helper_functions_one.c helper_functions_two.c \
+		initial_coders.c initial_dongles.c initial_heap.c initial_manager.c \
+		initial_monitor.c initializer.c join_threads.c manager.c monitor.c \
+		parser.c times.c
 OBJ = $(SRC:.c=.o)
 
 all: $(NAME)
--- destroy.c
+++ destroy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   destroy.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hel-hamo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: darwin <darwin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/30 00:56:29 by hel-hamo          #+#    #+#             */
-/*   Updated: 2026/03/30 00:56:31 by hel-hamo         ###   ########.fr       */
+/*   Updated: 2026/04/05 01:42:44 by darwin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,13 @@ void	destroy_mutexes_of_dongles_at(t_dongle *dongles, int i)
 
 void	destroy_mutexes_of_coders_at(t_coder *coders, int i)
 {
-	while (--i)
+	while (--i >= 0)
 		pthread_mutex_destroy(&coders[i].mutex_coder);
 }
 
 void	destroy_conds_of_coders_at(t_coder *coders, int i)
 {
-	while (--i)
+	while (--i >= 0)
 		pthread_cond_destroy(&coders[i].cond_coder);
 	free(coders);
 }
